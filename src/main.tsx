c5ca7026e5bfb053c9f1aa43e17a39d1c8fe0bfc
Mariana <mariana.souza09@outlook.com>
2025-05-03 14:27:29 -0300

    commit do codigo

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+/*import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(<App />);*/
+
+//Oque está comentado era o original
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+ReactDOM.createRoot(document.getElementById("root") as Element).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
